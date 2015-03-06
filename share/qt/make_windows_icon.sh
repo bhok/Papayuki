4a64b5d8079582c221cc4d8071dc55f8a88c0ca9
bhok <bhokor@gmail.com>
2015-03-06 13:43:41 +0100

    Update make_windows_icon.sh

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/Naanayam.ico
+ICON_DST=../../src/qt/res/icons/Papayuki.ico
 
-convert ../../src/qt/res/icons/Naanayam-16.png ../../src/qt/res/icons/Naanayam-32.png ../../src/qt/res/icons/Naanayam-48.png ${ICON_DST}
+convert ../../src/qt/res/icons/Papayuki-16.png ../../src/qt/res/icons/Papayuki-32.png ../../src/qt/res/icons/Papayuki-48.png ${ICON_DST}
